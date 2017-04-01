896d7570de4f2a1627f29a8e5d4a6c3ea30b63bb
Molly Arhammar <mopooy@gmail.com>
2017-04-01 12:18:14 +0000

    try sorting out static files

--- server/server.js
+++ server/server.js
@@ -24,8 +24,8 @@ app.set('port', port);
 //Middlewares-------------------------------------------------------------------------------------------------------
 
 //Find static resources.
-app.use(express.static(path.join(cwd, '../client/dist')));
-console.log(path.join(cwd + '../client/dist'));
+app.use(express.static(path.join(cwd, '/../client/dist')));
+console.log(path.join(cwd + '/../client/dist'));
 
 //JSON support
 app.use(bodyParser.json());
