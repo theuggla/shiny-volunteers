95cff7c43b4ae33b2a8fabfc348214bb2a3103f4
Molly Arhammar <mopooy@gmail.com>
2017-04-01 14:55:01 +0000

    fix travis deploy to heroku

--- client/source/js/app.jsx
+++ client/source/js/app.jsx
@@ -47,4 +47,4 @@ let HelloWorld = React.createClass({
 });
 
 
-ReactDOM.render(<HelloWorld greetTarget="Spiderman" />, document.getElementById('container'));
+ReactDOM.render(<HelloWorld greetTarget="Wonder Woman" />, document.getElementById('container'));
