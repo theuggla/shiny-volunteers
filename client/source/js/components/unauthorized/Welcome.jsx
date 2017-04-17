014b8e1f311efcb67b8309da572a81e6bba020e3
Molly Arhammar <mopooy@gmail.com>
2017-04-17 12:57:28 +0200

    finish up some styles

--- client/source/js/components/unauthorized/Welcome.jsx
+++ client/source/js/components/unauthorized/Welcome.jsx
@@ -4,8 +4,8 @@ import FlatButton from 'material-ui/FlatButton';
 const Welcome = ({ history }) => (
     <div className="welcome">
         <h3>spark the revolution</h3>
-        <FlatButton label="volunteer" onTouchTap={() => {history.push('/login/volunteer')}} />
-        <FlatButton label="find volunteers" onTouchTap={() => {history.push('/login/organization')}} />
+        <FlatButton className="button-line" label="volunteer" onTouchTap={() => {history.push('/login/volunteer')}} />
+        <FlatButton className="button-line" label="find volunteers" onTouchTap={() => {history.push('/login/organization')}} />
     </div>
 );
 
--- client/source/js/containers/unauthorized/FrontEndContainer.jsx
+++ client/source/js/containers/unauthorized/FrontEndContainer.jsx
@@ -9,7 +9,7 @@ import About from '../../components/unauthorized/About.jsx';
 
 
 const Container = ({ match }) => (
-            <div className="front-end" style={{color: 'white', height: '100vh', width: '100vw', backgroundImage: `url(${Background})`}}>
+            <div className="front-end app-container" style={{color: 'white', height: '100vh', width: '100vw', backgroundImage: `url(${Background})`}}>
                 <div className="top-bar">
                         <Link id="about-link" to="/about">About</Link>
                         <Link id="start-link" to="/">Shiny App</Link>
