fa5d875e728cb47264f2d92015c7f0b62e81f07a
Molly Arhammar <mopooy@gmail.com>
2017-04-29 11:36:30 +0200

    change email server

--- server/lib/emailresource.js
+++ server/lib/emailresource.js
@@ -5,28 +5,28 @@
 
 // Requires.
 let nodemailer = require('nodemailer');
-let mailgun = require('nodemailer-mailgun-transport');
+let sendgrid = require('nodemailer-sendgrid-transport');
 
 // Configs.
 let auth = {
     auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.MAILGUN_DOMAIN
+        api_key: process.env.SENDGRID_PASSWORD,
+        api_user: process.env.SENDGRID_USERNAME
     }
 };
 
-let mailsender = nodemailer.createTransport(mailgun(auth));
+let mailsender = nodemailer.createTransport(sendgrid(auth));
 
 
 module.exports.sendMail = function() {
+    let email = {
+        from: 'do-not-reply@spark-the-revolution.com',
+        to: ['mopooy@gmail.com'],
+        subject: 'Hey you, awesome sendgridder!',
+        html: '<b>Wow Big powerful letters</b>'
+    };
     return new Promise((resolve, reject) => {
-        mailsender.sendMail({
-                from: 'do-not-reply@spark-the-revolution.com',
-                to: 'mopooy@gmail.com',
-                subject: 'Hey you, awesome!',
-                html: '<b>Wow Big powerful letters</b>'},
-
-            (err, info) => {
+        mailsender.sendMail(email, (err, info) => {
                 if (err) {
                     console.log('Error: ' + err);
                     reject(err);
