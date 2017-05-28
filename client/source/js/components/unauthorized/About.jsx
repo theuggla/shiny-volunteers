99cf792bedceb440313357ec1470af08b684d03d
Molly Arhammar <mopooy@gmail.com>
2017-05-28 11:59:51 -0700

    clean up tests

--- client/source/js/components/unauthorized/About.jsx
+++ client/source/js/components/unauthorized/About.jsx
@@ -10,7 +10,8 @@ import React from 'react';
  * Renders an about-text.
  */
 const About = () => (
-    <div className="about"><p>Want to help out? Need help?</p></div>
+    <div className="about"><p>Want to help out? Need help? Spark is a free service to match volunteer-bases organizations with people who wants to help. Register as a volunteer, let us know how often you want to help
+        and what kind of skills and interest you have and get a list with needs matching your profile where you can apply with a single click, or register as an organization, fill out your needs, and get an email with contact info when someone shows interest.</p></div>
 );
 
 // Exports ------------------------------------------------------------------------------------------------------------
