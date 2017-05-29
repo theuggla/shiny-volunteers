108312ef91cd91e1869afd044c8a16a74ee76fe9
Molly Arhammar <mopooy@gmail.com>
2017-05-29 12:35:21 -0700

    update about-text

--- client/source/js/components/unauthorized/About.jsx
+++ client/source/js/components/unauthorized/About.jsx
@@ -12,7 +12,7 @@ import styles from '../../ReactStyles'
  * Renders an about-text.
  */
 const About = () => (
-    <div className="about" style={styles.about}><p>Want to help out? Need help? Spark is a free service to match volunteer-bases organizations with people who wants to help. Register as a volunteer, let us know how often you want to help
+    <div className="about" style={styles.about}><p>Want to help out? Need help? Spark is a free service to match volunteer-based organizations with people who wants to help. Register as a volunteer, let us know how often you want to help
         and what kind of skills and interest you have and get a list with needs matching your profile where you can apply with a single click, or register as an organization, fill out your needs, and get an email with contact info when someone shows interest.</p></div>
 );
 
