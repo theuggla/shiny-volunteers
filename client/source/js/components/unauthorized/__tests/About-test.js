1f4799268fdd7bd9fe9dfb54f97686e721562d45
Molly Arhammar <mopooy@gmail.com>
2017-05-29 12:35:40 -0700

    update tests

--- client/source/js/components/unauthorized/__tests/About-test.js
+++ client/source/js/components/unauthorized/__tests/About-test.js
@@ -13,7 +13,7 @@ describe("About Component", () => {
     });
 
     it("should contain text", (done) => {
-        expect(shallow(<About />).contains(<p>Want to help out? Need help? Spark is a free service to match volunteer-bases organizations with people who wants to help. Register as a volunteer, let us know how often you want to help
+        expect(shallow(<About />).contains(<p>Want to help out? Need help? Spark is a free service to match volunteer-based organizations with people who wants to help. Register as a volunteer, let us know how often you want to help
             and what kind of skills and interest you have and get a list with needs matching your profile where you can apply with a single click, or register as an organization, fill out your needs, and get an email with contact info when someone shows interest.</p>)).to.equal(true);
         done();
     });
