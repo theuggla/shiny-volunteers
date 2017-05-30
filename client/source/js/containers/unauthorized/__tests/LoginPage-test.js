40f3a5edcaffb9e86fbc7a1d78ac4bc317d48557
Molly Arhammar <mopooy@gmail.com>
2017-05-29 18:21:56 -0700

    try to fix random disconnect test

--- client/source/js/containers/unauthorized/__tests/LoginPage-test.js
+++ client/source/js/containers/unauthorized/__tests/LoginPage-test.js
@@ -154,7 +154,7 @@ describe("LoginPage", () => {
         step("Submit the form", function(done) {
             const form = wrapper.find(".login-form");
             form.simulate("submit");
-            done();
+            setTimeout(done, 1000);
         });
 
         step("Display error message", function(done) {
